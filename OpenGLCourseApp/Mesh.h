6469458817c2a57789369867b8b46d6349ce9ee3
IsseiMori <ganntyu1225issei@gmail.com>
2019-03-17 12:53:24 -0700

    ini commit

---
+++ OpenGLCourseApp/Mesh.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <GL/glew.h>
+class Mesh
+{
+public:
+	Mesh();
+
+	void CreateMesh(GLfloat *vertices, unsigned int *indices, unsigned int numofVertices, unsigned int numofIndices);
+	void RenderMesh();
+	void CleanMesh();
+
+	~Mesh();
+
+private:
+	GLuint VAO, VBO, IBO;
+	GLsizei indexCount;
+};
+
